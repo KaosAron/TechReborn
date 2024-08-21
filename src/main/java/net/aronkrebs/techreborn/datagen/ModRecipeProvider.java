bfbb7b4b99a58f3530d517eba14ccec932005bd4
KaosAron <krebs.aron@gmail.com>
2024-08-21 18:14:42 +0200

    Recipe Datagen first half

--- src/main/java/net/aronkrebs/techreborn/datagen/ModRecipeProvider.java
+++ src/main/java/net/aronkrebs/techreborn/datagen/ModRecipeProvider.java
@@ -1,4 +1,34 @@
 package net.aronkrebs.techreborn.datagen;
 
-public class ModRecipeProvider {
+import net.aronkrebs.techreborn.block.ModBlocks;
+import net.aronkrebs.techreborn.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.recipe.book.RecipeCategory;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    private static final List<ItemConvertible> LITHIUM_SMELTABLES = List.of(ModBlocks.LITHIUM_ORE,
+            ModBlocks.DEEPSLATE_LITHIUM_ORE, ModItems.RAW_LITHIUM);
+
+    public ModRecipeProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, LITHIUM_SMELTABLES, RecipeCategory.MISC, ModItems.LITHIUM_INGOT,
+                0.7f, 200, "lithium");
+        offerBlasting(exporter, LITHIUM_SMELTABLES, RecipeCategory.MISC, ModItems.LITHIUM_INGOT,
+                0.7f, 200, "lithium");
+
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.LITHIUM_INGOT,
+                RecipeCategory.DECORATIONS, ModBlocks.LITHIUM_BLOCK);
+
+
+    }
 }
