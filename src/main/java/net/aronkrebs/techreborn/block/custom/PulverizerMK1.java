8843bfcfbc70121c3edeb154e24f2a58d9f8275a
KaosAron <krebs.aron@gmail.com>
2024-08-21 19:26:41 +0200

    Datagen completed!
    Beginning with the first Machine...
    THE PULVERIZER

---
+++ src/main/java/net/aronkrebs/techreborn/block/custom/PulverizerMK1.java
@@ -0,0 +1,20 @@
+package net.aronkrebs.techreborn.block.custom;
+
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
+public class PulverizerMK1 extends BlockWithEntity implements BlockEntityProvider {
+    public PulverizerMK1(Settings settings) {
+        super(settings);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return null;
+    }
+}
--- src/main/java/net/aronkrebs/techreborn/datagen/ModRecipeProvider.java
+++ src/main/java/net/aronkrebs/techreborn/datagen/ModRecipeProvider.java
@@ -5,8 +5,11 @@ import net.aronkrebs.techreborn.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -29,6 +32,14 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.LITHIUM_INGOT,
                 RecipeCategory.DECORATIONS, ModBlocks.LITHIUM_BLOCK);
 
-
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.BATTERY, 1)
+                .pattern(" L ")
+                .pattern("LIL")
+                .pattern("LIL")
+                .input('L', ModItems.LITHIUM_INGOT)
+                .input('I', Items.IRON_INGOT)
+                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .criterion(hasItem(ModItems.LITHIUM_INGOT), conditionsFromItem(ModItems.LITHIUM_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.BATTERY)));
     }
 }
